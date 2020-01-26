64e07f869ea7492ccdad644c0c4a4ae902991771
hjstam1962 <hansjanke@outlook.com>
2020-01-26 10:13:28 +0100

    Up to 3: Creating Your...

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core'
+import { EventEmitter } from 'protractor';
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+        <div class="well hoverwell thumbnail"> 
+            <h2>{{event.name}}</h2>
+            <div>Date: {{event.date}}</div>
+            <div>Time: {{event.time}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location: {{event.location.address}}</span>
+                <span>&nbsp;</span>
+                <span>{{event.location.city}}, {{event.location.country}}</span>
+            </div>
+            <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+        </div>    
+    `
+})
+export class EventThumbnailComponent {
+    @Input() event:any
+
+    handleClickMe() {
+        console.log('clicked')
+    }
+}
